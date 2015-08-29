9bfb28d464b0681c029ef7456981d5c93a2823f3
Noah Gordon <noah.zachary.gordon@gmail.com>
2015-08-29 16:56:07 -0400

    Start work on more folds [week 4]

---
+++ week_4/more_folds.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wall #-}
+
+xor :: [Bool] -> Bool
+xor = foldr xor' False
+  where xor' x y = (x || y) && (not (x && y))
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr ((:) . f) []
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base xs = undefined
